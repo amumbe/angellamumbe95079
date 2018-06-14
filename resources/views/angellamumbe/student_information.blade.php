2aeef503a54cdd5737d9a5df8f978e33ba196c98
unknown <angellamumbe720@gmail.com>
2018-06-14 09:48:23 +0300

    final

--- resources/views/angellamumbe/student_information.blade.php
+++ resources/views/angellamumbe/student_information.blade.php
@@ -13,8 +13,8 @@
 
             <div class="col-lg-6">
                 <h1>Student Details</h1>
-                <h4>Student_id: {{$student_id}}<br></h4>
-                <h4>Full_name:  {{$full_name}}<br></h4>
+                <h4>Student_id: {{$student_number}}<br></h4>
+                <h4>Full_name:  {{$student_name}}<br></h4>
 
                 <h1>Fees Transaction</h1>
                     <table class="table table-hover">
@@ -27,7 +27,7 @@
 
                         @foreach ($student as $students)
                             <tr>
-                                <td>{{$students->student_id}}</td>
+                                <td>{{$students->student_number}}</td>
                                 <td>{{$students->payment_id}}</td>
                                 <td>{{$students->date_paid}}</td>
                                 <td>{{$students->amount_paid}}</td>
